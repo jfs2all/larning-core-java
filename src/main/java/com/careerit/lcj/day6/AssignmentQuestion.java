e9ec958c8c497cf1a53843992a0eccf8c4dd154e
jfs <jfs2all@gmail.com>
2023-12-19 08:33:38 +0530

    Added assignment question

---
+++ src/main/java/com/careerit/lcj/day6/AssignmentQuestion.java
@@ -0,0 +1,7 @@
+package com.careerit.lcj.day6;
+
+// create a class with name student -> id,name,std,fee,age and create 5 objects and print the details of the students
+
+
+public class AssignmentQuestion {
+}
